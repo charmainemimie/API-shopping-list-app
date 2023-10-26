26938a84cdf6c63e2ff7adfd981f4ea9b72dda32
charmainemimie <82668352+charmainemimie@users.noreply.github.com>
2023-10-26 21:47:51 +0100

    created first endpoint - GET(READ)

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const app=require('express')();
 const PORT =8080;
 
 //start api 
- app.listen(
+/* app.listen(
     PORT, 
     ()=>console.log(`it is running on http://localhost:${PORT}`)
-) 
+) */
+
+//endpoint 1 (READ)
+app.get('/tshirt', (req, res)=>{
+    //respond okay and send a data payload along with the response which is the tshirt and size
+    res.status(200).send({
+        tshirt:'👕',
+        size:'large'
+    })
+});
+
