dcb98b0006c1dccc30d145be8283446430d65b21
charmainemimie <82668352+charmainemimie@users.noreply.github.com>
2023-10-26 22:41:21 +0100

    created second endpoint - POST(CREATE)

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 //api name
-const app=require('express')();
+const express=require('express');
+const app = express();
 const PORT =8080;
 
+app.use(express.json())
 //start api 
-/* app.listen(
+app.listen(
     PORT, 
     ()=>console.log(`it is running on http://localhost:${PORT}`)
-) */
+) 
 
 //endpoint 1 (READ)
 app.get('/tshirt', (req, res)=>{
     //respond okay and send a data payload along with the response which is the tshirt and size
     res.status(200).send({
         tshirt:'👕',
-        size:'large'
+        size:'large',
     })
 });
 
+//endpoint 2 (CREATE)
+app.post('/tshirt/:id', (req, res)=>{
+//url with a dynamic parameter which is the id of the tshirt
+    const { id } = req.params;
+    const {logo} = req.body;
+
+    if(!logo){
+        res.status(418).send({message: 'We need a logo!'})
+    }
+    res.send({
+        tshirt: `👕 with your ${logo} and ID of ${id}`
+    })
+}); 
