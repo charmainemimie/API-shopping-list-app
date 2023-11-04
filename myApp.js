1d922b4d7cf780416289dd7ca4d0aa432ee40c3e
charmainemimie <82668352+charmainemimie@users.noreply.github.com>
2023-11-04 14:51:35 +0100

    used GET with JSON object

--- myApp.js
+++ myApp.js
@@ -8,8 +8,12 @@ console.log("Hello World")
 
 app.use("/public",express.static(__dirname + '/public'));
 
-app.get('/', (req, res)=>{
+/* app.get('/', (req, res)=>{
     res.sendFile(__dirname+'/views/index.html');
+}); */
+
+app.get('/json', (req, res)=>{
+    res.json({"message": "Hello json"})
 });
 
 
