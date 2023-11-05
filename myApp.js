7d14bc9bacb01a320a97abdfd5f78091d3ab00bb
charmainemimie <82668352+charmainemimie@users.noreply.github.com>
2023-11-05 11:45:30 +0100

    chained middleware to a route

--- myApp.js
+++ myApp.js
@@ -7,18 +7,22 @@ require('dotenv').config()
         res.send("Hello Express");
 }) */
 
+//middleware1....serving static resources
 app.use("/public",express.static(__dirname + '/public'));
 
+//middleware2
 app.use((req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
 })
 
+//endpoint 1
 /* app.get('/', (req, res)=>{
     res.sendFile(__dirname+'/views/index.html');
 }); */
 
-app.get('/json', (req, res)=>{
+//endpoint 2
+/* app.get('/json', (req, res)=>{
     //res.json({"message": "Hello json"})
      if(process.env.MESSAGE_STYLE === 'uppercase'){
         res.json({"message": "HELLO JSON"})
@@ -34,9 +38,26 @@ app.get('/json', (req, res)=>{
         }
         res.json({
             "message": response
-        }); */
-});
+        }); 
+}); */
 
+//endpoint 3...with middleware
+app.get('/now',(req,res,next) => {
+    //middleware
+    // adding a new property to req object
+    // in the middleware function
+    req.time = new Date().toString();
+    next();
+},
+
+
+    //route body
+    // accessing the newly added property
+    // in the main function
+    (req,res) => {
+        res.json({time: req.time})
+    }
+);
 
 
 
